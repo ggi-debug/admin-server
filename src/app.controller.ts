8ec2b64e81f938f9eaa341afa8d93ebe8a216b09
张腾飞 <ztengfei058@gmail.com>
2023-03-19 17:45:25 +0800

    提交

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,6 +1,6 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
-
+//测试推送
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
